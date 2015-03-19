fd71e88ad05e65faad44db610d014100b517a0a4
Arthur Silber <arthur@arthursilber.de>
2015-03-19 14:41:31 +0100

    fixed three.js to let lowfab tests run smoothly

--- build/three.js
+++ build/three.js
@@ -27273,8 +27273,7 @@ THREE.FontUtils.generateShapes = function ( text, parameters ) {
 } )( THREE.FontUtils );
 
 // To use the typeface.js face files, hook up the API
-self._typeface_js = { faces: THREE.FontUtils.faces, loadFace: THREE.FontUtils.loadFace };
-THREE.typeface_js = self._typeface_js;
+THREE.typeface_js = { faces: THREE.FontUtils.faces, loadFace: THREE.FontUtils.loadFace };
 
 // File:src/extras/audio/Audio.js
 
